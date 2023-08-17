551ddfaabda1f7221fa9e8d2151e6440cba97c64
baser5doruq <dorukozgocmen6@gmail.com>
2023-08-17 23:10:57 +0300

    meme
    a

---
+++ Assets/Rotate.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+
+    [SerializeField]
+    public float degrees = 0f;
+    public float increase = 20f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        degrees += increase;
+        transform.eulerAngles = Vector3.forward * degrees;
+    }
+}
